b8d6396ab505be9116a7c137d25fdafc9090a3ce
baongoc2211 <hoavienchon@gmai.com>
2025-06-01 22:14:27 +0700

    feat: SCRUM-74 Screen Sign Up

--- src/api/auth.api.ts
+++ src/api/auth.api.ts
@@ -1,7 +1,11 @@
-import { ISignIn } from "@/interface/auth/admin.interface";
+import { ISignIn, ISignUp } from "@/interface/auth/admin.interface";
 import APIConfig from "./api.config";
 
 export const signInAPI = async (data: ISignIn) => {
   const res = await APIConfig.post("/api/admin/sign-in", data);
+  return res.data;
+};
+export const signUpAPI = async (data: ISignUp) => {
+  const res = await APIConfig.post("/api/admin/sign-up", data);
   return res.data;
 };
